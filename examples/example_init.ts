d05fc62e2bd10bcf356cf9a28115fd3b19780c35
David Matthews <david@matthews-tung.com>
2025-06-08 15:36:36 +1000

    Issue 23: Fix request/response logging & improve documentation

---
+++ examples/example_init.ts
@@ -0,0 +1,29 @@
+/*
+
+How to run this example:
+
+1. From the root folder build the library: npm run build
+2. Change to the examples directory: cd examples
+3. Change the address and password to match your RainBird controller.
+4. Build the example: npm run build
+5. Run the example: npm start
+
+*/
+import { RainBirdService, LogLevel, EventType } from '../dist/index.js';
+
+// Create the RainBird service
+const rainbird = new RainBirdService({
+    address: '<0.0.0.0>', // Replace with your RainBird controller's IP address
+    password: '<password>', // Replace with your RainBird controller's password
+    refreshRate: 90,
+    showRequestResponse: true,
+    syncTime: true,
+});
+
+rainbird.on(EventType.LOG, (log) => {
+    console.log(`${log.level}: ${log.message}`);
+});
+
+var metadata = await rainbird.init();
+
+console.log('RainBird Metadata:', metadata);
--- src/RainBird/RainBirdService.ts
+++ src/RainBird/RainBirdService.ts
@@ -87,10 +87,8 @@ export class RainBirdService extends events.EventEmitter {
     this._syncTime = options.syncTime
 
     this._client = new RainBirdClient(options.address, options.password, options.showRequestResponse)
-    this._client.on(EventType.LOG, (level: LogLevel, message: string) => {
-      if (message !== undefined) {
-        this.emitLog(level, message)
-      }
+    this._client.on(EventType.LOG, (log) => {
+      this.emitLog(log.level, log.message)
     })
 
     this._statusRefreshSubject
